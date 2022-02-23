c47f974aec81973eb0ea33d15184d90fac6d7878
omerokumus <omerokumus3@gmail.com>
2022-02-23 12:40:13 +0300

    if-else continued

---
+++ Ch2_3_CommonErrors/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Ch2_3_CommonErrors
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Errors
+            // 1- Forgetting Necessary Braces
+            double radius = -2.3;
+            double area = -1;
+            if (radius >= 0)
+                area = 3.14 * radius * radius;
+                Console.WriteLine("area is " + area);
+
+                
+            // 2- Wrong semicolon at the end of if
+            if (2 < 3) ;
+            
+            
+            // 3- Redundant boolean value testing
+            bool b = 2 < 3;
+            if (b == true)
+            {
+                
+            }
+
+            if (b)
+            {
+                
+            }
+            
+            // use case
+            if ("a".Equals("b"))
+            {   
+                
+            }
+            
+            
+            // 5- Equality test in floating point numbers
+            if (10000000000.0 - 0.0000001 == 9999999999.9999999 ) // kötü sonuçlar doğurabilir :)
+            {
+                Console.WriteLine("xxxx");
+            }
+            
+            
+            // Pitfalls
+            // 1- Simplifying if else statements
+            int number = 4;
+            if (number % 2 == 0) // number % 2 == 0 true is b true olur
+            {                   
+                b = true;
+            }
+            else // number % 2 == 0 false ise b false olur
+            {
+                b = false;
+            }
+
+            // IF'in içi b'nin alacağı değer ile aynı ise bu şekilde sadeleştirilir
+            b = number % 2 == 0; // yukarıdaki if else ile aynı şey
+
+            if (number % 2 == 0)
+            {
+                b = false;
+            }
+            else
+            {
+                b = true;
+            }
+
+            // IF'in içi b'nin alacağı değerin tersi ise bu şekilde tersini b'ye atayarak sadeleştirme yapılabilir.
+            b = number % 2 != 0;
+            b = !(number % 2 == 0);
+            
+            
+            
+            // 2- Code duplication
+            int salary;
+            if (b)
+            {
+                salary = 1000;
+                Console.WriteLine("salary: " + salary); // duplicated
+            }
+            else
+            {
+                salary = 1500;
+                Console.WriteLine("salary: " + salary); // duplicated
+            }
+            
+            // düzeltmesi: duplicated satırlar her koşulda çalıştığından ötürü if-else yapısının dışına alınır
+            if (b)
+            {
+                salary = 1000;
+            }
+            else
+            {
+                salary = 1500;
+            }
+            Console.WriteLine("salary: " + salary); 
+
+            
+            
+
+        }
+    }
+}
---
+++ Ch2_Exercise1_BMI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ch2_Exercise1_BMI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+                BMI < 18.5 Underweight
+                18.5 ≤ BMI < 25.0 Normal
+                25.0 ≤ BMI < 30.0 Overweight
+                30.0 ≤ BMI Obese
+             */
+            
+            /* TODO:
+             *      1. Kullanıcan kilo ve boy değerlerini al: kilo kg cinsinden, boy metre cinsinden
+             *      2. BMI Hesapla: kilo / (boy * boy)
+             *      3. BMI değerine göre sonucu ekrana bastır
+             */
+
+            // 1. Kullanıcan kilo ve boy değerlerini al: kilo kg cinsinden, boy metre cinsinden
+            Console.WriteLine("Enter weight in kg");
+            double weight;
+            Double.TryParse(Console.ReadLine(), out weight);
+            
+            Console.WriteLine("Enter height in meters");
+            double height;
+            Double.TryParse(Console.ReadLine(), out height);
+            
+            // 2. BMI Hesapla: kilo / (boy * boy)
+            double bmi = weight / (height * height);
+            // double bmi = weight / Math.Pow(height, 2); // 2. yöntem
+    
+            // 3. BMI değerine göre sonucu ekrana bastır
+            Console.WriteLine("BMI: " + Math.Round(bmi, 2));
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if (bmi < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi < 30)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Obese");
+            }
+
+        }
+    }
+}
