98342c740d2a048353ee02f8e499c66a893af1b5
omerokumus <omerokumus3@gmail.com>
2022-03-09 15:21:57 +0300

    do-while and for loops

---
+++ Ch3_4_DoWhileLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ch3_4_DoWhileLoop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // do-while loop
+            // Temelde bir döngüdür. while döngüsünden farkı do-while döngüsünün en az 1 kere çalışıyor olmasıdır.
+            // while döngüsü önce kontrol eder sonra çalıştırır. do-while döngüsü önce çalıştırır sonra kontrol eder
+            // Bir kodun veya kod satırlarının en az 1 kere çalışmasını istiyorsak do-while kullanmalıyız
+
+
+            int n;
+            int loopCounter = 1;
+            bool isCorrect = true;
+            do
+            {
+                // if (loopCounter > 1)
+                //     Console.WriteLine("Invalid Input. Try again.");
+                
+                if (!isCorrect)
+                    Console.WriteLine("Invalid input. Try again.");
+                Console.Write("Enter number: ");
+                Int32.TryParse(Console.ReadLine(), out n);
+                Console.WriteLine();
+                // loopCounter++;
+                isCorrect = false;
+            } while (!(0 <= n && n <= 9));
+            Console.WriteLine("Your number is " + n);
+
+        }
+    }
+}
---
+++ Ch3_5_ForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ch3_5_ForLoop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // For döngüsü: while döngüsünden bir farkı yoktur. Sadece daha derli toplu bir yapı oluşturur.
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
+            
+            
+            // while to for
+            // int i = 0; // for parantezlerindeki ilk statement
+            // while (i < 10) // 2. statement
+            // {
+            //     Console.WriteLine(i);
+            //     i++; // 3. statement
+            // }
+            
+            // Ne zaman while?: sentinel controlled loop oluşturmak için while döngüsü daha kullanışlı
+            // Ne zaman for?: counter controlled loop oluşturmak için for döngüsü daha iyi
+            
+            
+            
+            
+            
+        }
+    }
+}
