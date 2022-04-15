1cfcbb6c116be1c82c63aae8d5be6ad3c65f0269
omerokumus <omerokumus3@gmail.com>
2022-04-15 15:06:36 +0300

    methods intro

---
+++ Ch4_1_MethodsIntro/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ch4_1_MethodsIntro
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Math.Abs(-2);
+            "abc".Remove('a');
+
+            /* Fonksiyon avantajları
+             *      1. Kodumuzu modülarize/organize eder
+             *      2. Kodun okunulabilirliğini arttırır
+             *      3. Code reusability artar (Kodun tekrar kullanılabilirliği)
+             *      4. Kod üzerindeki kontrol artar
+             *      5. Hatalar daha kolay bulunur
+             *      6. Kodların birbirine olan bağımlılığı azalır
+             */
+            
+            /*  İş terimi: Gerçek hayatta yaptığımız iş anlamına gelir. Örn; markete gitme işi,
+             *      yemek yapma işi, ev temizleme işi, ödev yapma işi vs.
+             *      Yazılımdaki örnekler;
+             *          - bir stringden bir karakteri silme işi (Remove fonksiyonu)
+             *          - Verilen bir stringi konsola yazma işi (Console.WriteLine)
+             *          - Verilen bir sayının pozitifini alma işi (Math.Abs)
+             *          - Verilen iki stringin aynı olup olmadığını kontrol etme işi (Equals)
+             *
+             *      İşler alt işlerden oluşabilir. Örn;
+             *                      Ev temizleme işi = Bulaşık yıkama işi + evi süpürme işi 
+             *      
+             *  İş terimi ne işimize yarar?
+             *      İşler methodlar yardımıyla halledilir. Bu sebeple eğerki bir iş yapılacaksa
+             *      o işi yapan fonksiyon yazılmalı. Yani iş terimi ile ne zaman fonksiyon yazılmalı
+             *      sorusuna cevap buluruz. 
+             */
+            
+            /*  Fonksiyon isimlendirme kuralları
+             *      - Her bir fonksiyon bir iş yapar. Bundan sebep Fonksiyonun adı yaptığı işin ne
+             *          olduğunu tanımlamalı
+             *      Örn; Remove -> silme işi yapar
+             *          Math.Abs -> mutlak değer alır
+             *          Math.Round -> sayıyı yuvarlar
+             *          Math.Floor -> sayıyı alttaki tam sayıya yuvarlar
+             *          Math.Pow -> sayıyının kuvvetini alır
+             *
+             *      - Fonksiyon isimleri fiil olmalıdır. (Her zaman değil)
+             *          Örn;
+             *              findUppercaseLetters(str) -> stringdeki büyük harfleri bulur
+             *              findDigitsOf(str) -> stringdeki rakamları bulur
+             */
+        }
+    }
+}
---
+++ Ch4_2_MethodDefinitionAndCalling/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+
+namespace Ch4_2_MethodDefinitionAndCalling
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*  Fonksiyon tipleri:
+             *      1- Çağıran kişiye (fonksiyon) bir şey geri vermeyen
+             *          Bu fonksiyonlar sadece işinin bittiğinin haberini kendisi çağırana verir
+             *      2- Çağıran kişiye (fonksiyon) bir şey geri veren
+             *          Bu fonksiyonlar hem işinin bittiğinin haberini hem de kendisini çağırana bir şey verir
+             */
+            
+            /*  Fonksiyon tanımlama formülü
+             *      1- Çağıran kişiye (fonksiyon) bir şey geri vermeyen
+             *          public static void FonksiyonAdı(parametreListesi)
+             *          {
+             *              fonksiyon body'si (function body) kodların bulunduğu yer
+             *          }
+             *
+             *      2- Çağıran kişiye (fonksiyon) bir şey geri veren
+             *          public static returnType FonksiyonAdı(parametreListesi)
+             *          {
+             *              fonksiyon body'si (function body) kodların bulunduğu yer
+             *          }
+             */
+            
+            // Function call/invocation
+            AddTwoIntegers(2, 3); // 2, 3 sayılarına arguman denir 
+            AbsoluteValue(-3);
+            AbsoluteValue(8);
+            SubtractThreeIntegers(10, 3, 5);
+
+            int sum = AddTwoIntegers2(10, 4);
+            Console.WriteLine("sum2: " + sum);
+
+            int res = SubtractThreeIntegers2(5, 1, 1);
+            Console.WriteLine("subtract: " + res);
+
+            int abs = AbsoluteValue2(-11);
+            Console.WriteLine("abs: " + abs);
+            
+            Console.WriteLine("abs: " + AbsoluteValue2(-77));
+        }
+        
+        // 1- Çağıran kişiye (fonksiyon) bir şey geri vermeyen
+        public static void AddTwoIntegers(int n1, int n2) // n1, n2 değişkenlerine parametre denir
+        {
+            int sum = n1 + n2;
+            Console.WriteLine("sum: " + sum);
+        }
+        
+        // 2- Çağıran kişiye (fonksiyon) bir şey geri veren
+        public static int AddTwoIntegers2(int n1, int n2)
+        {
+            int sum = n1 + n2;
+            return sum;
+        }
+        public static void SubtractThreeIntegers(int n1, int n2, int n3)
+        {
+            int res = n1 - n2 - n3;
+            Console.WriteLine("result: " + res);
+        }
+
+        public static int SubtractThreeIntegers2(int n1, int n2, int n3)
+        {
+            int res = n1 - n2 - n3;
+            return res;
+        }
+
+        public static void AbsoluteValue(int n)
+        {
+            int res = (n < 0) ? -n : n;
+            Console.WriteLine("Absolute value of " + n + " is " + res);
+        }
+
+        public static int AbsoluteValue2(int n)
+        {
+            int res = (n < 0) ? -n : n;
+            return res;
+        }
+
+    }
+}
