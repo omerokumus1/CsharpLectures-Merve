81dcaceab0c9a60980e435741b65ffcd5033f032
Ömer Faruk Okumuş <76251918+omerokumus1@users.noreply.github.com>
2022-06-27 16:26:46 +0300

    Add files via upload

---
+++ Ch5_1_ArraysBasics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ch5_1_ArraysBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrayler indexlenebilir yapılardır. Stringlerdeki gibi belirli
+            // indexteki elemana erişebiliriz
+
+            // 1- Array Declaration
+            // dataType[] arrayName; -> dataType: int, double, string, boolean ...
+            // dataType: dizinin tutacağı elemanların tipidir. int ise dizi içinde sadece int eleman olabilir
+            int[] numbers;
+
+
+            // 2- Array Initialization (memory allocation for arrays)
+            // arrayName = new dataType[SIZE]; -> new zorunludur. SIZE ile arrayin boyutu belirlenir
+            numbers = new int[5];
+            
+
+            // 3- Array Declaration and Initialization in One Line
+            // dataType[] arrayName = new dataType[SIZE];
+            bool[] bools = new bool[5];
+
+
+            // 4- Dizinin bir elemanına erişmek
+            // arrayName[index]
+            Console.WriteLine(numbers[2]); // default 0 basar
+            Console.WriteLine(bools[2]); // default False basar
+
+            // new ile dizi oluşturulunca bütün elemanlar data tipinin default değerine setlenir
+            // int için default değer 0'dır. Yani bütün elemanlar sıfır değerine sahip olur
+            // bool için False'dir.
+            // Bu şekilde tüm data tipler için default değerler mevcuttur
+
+
+            // 4.1- Dizinin son elemanına erişmek
+            Console.WriteLine(bools[bools.Length - 1]);
+            Console.WriteLine(bools[^1]); // ^1 -> son index
+
+            // 5- Dizinin bir elemanını değiştirmek
+            // arrayName[index] = e;
+            numbers[0] = 11;
+            Console.WriteLine(numbers[0]);
+
+
+            // 6- Array Initialization with Elements
+            double[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(arr[2]);
+
+
+        }
+    }
+}
+
---
+++ Ch5_3_ProcessingArrays/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Ch5_3_ProcessingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InitializeArrayWithInputValues();
+
+            //int[] numbers = InitializeArrayWithRandomValues();
+            //PrintArray(numbers);
+
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //Console.WriteLine("sum: " + SumAllElements(numbers));
+
+            int[] numbers = { 1, 7, 2, 3, 6, 9, 11, 25, 14, 10 };
+            Console.WriteLine("max: " + FindLargestElement(numbers));
+        }
+
+        // 5. Finding the largest element
+        public static int FindLargestElement(int[] arr)
+        {
+            int max = arr[0];
+            foreach (var item in arr)
+            {
+                if (item > max)
+                    max = item;
+            }
+            return max;
+        }
+
+        // 4. Summing all elements
+        public static int SumAllElements(int[] arr) // 1 2 3 4 5
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        // 2. Initializing arrays with random values
+        public static int[] InitializeArrayWithRandomValues()
+        {
+            int[] numbers = new int[5];
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(10); // [0 - 10)
+            }
+            return numbers;
+        }
+
+        // 1. Initializing arrays with input values:
+        public static void InitializeArrayWithInputValues()
+        {
+            int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter an integer: ");
+                Int32.TryParse(Console.ReadLine(), out numbers[i]);
+                Console.WriteLine();
+            }
+            PrintArray(numbers);
+        }
+
+        // 3. Print array
+        public static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
+
